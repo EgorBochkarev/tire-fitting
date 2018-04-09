ca94a307c19ed8c08e691b88de00cbe4dd74448b
Vlad <templerock@mail.ru>
2018-04-09 13:03:08 +0300

    repository

---
+++ src/main/java/com/netcracker/repository/AuthorizationAppRepository.java
@@ -0,0 +1,10 @@
+package com.netcracker.repository;
+
+import com.netcracker.jpa.AuthorizationApp;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorizationAppRepository extends CrudRepository<AuthorizationApp, Integer>, JpaSpecificationExecutor<AuthorizationApp> {
+}
